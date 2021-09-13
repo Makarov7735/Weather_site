371b006768e5407d87588322acce5e51ac2840ed
Makarov7735 <85968962+Makarov7735@users.noreply.github.com>
2021-09-13 22:37:58 +0300

    Delete weather_site directory

--- weather_site/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import Weather_api
-
-urlpatterns = [
-    path('', Weather_api.as_view())
-]
--- weather_site/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import Weather_by_city
-
-
-urlpatterns = [
-    path('', Weather_by_city.as_view(), name='default_p'),
-]
